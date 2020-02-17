b09d7deb6888e669dae74af76de3c211a28c5c1d
Jeffrey Pearce <jeffikus@gmail.com>
2020-02-17 09:40:43 +0200

    Build tool test ready to deploy to npm.

---
+++ build-tool.sh
@@ -0,0 +1,43 @@
+# Clear the screen
+clear
+
+# UI init
+echo "===================================================="
+echo "|             --  WordPress Design  --             |"
+echo "|              -- Varya Build Tool --              |"
+echo "|           --     Version 1.0.0      --           |"
+echo "===================================================="
+echo "Enter the name of your new theme:"
+
+# Get Child Theme Name from user
+read rawthemename
+
+# Make sure the theme name is lowercase and replace whitespace with dashes
+themename=$(echo "$rawthemename" | tr '[:upper:]' '[:lower:]')
+themename=${themename// /-};
+themenamevariation=${themename//-/_};
+themeuppervariation=${rawthemename// /_};
+
+# Make Child Theme directory
+mkdir -p ../../$themename/sass
+echo "Creating child theme in folder: $themename"
+
+# Copy SASS and package files from Design System
+echo "Building your child theme...."
+
+# Find and replace - names and descriptions
+# Find and replace - file paths
+# Find and replace - main stylesheet
+# Find and replace - editor stylesheet
+# Find and replace - print stylesheet
+# Find and replace - package file
+# Find and replace - functions file
+
+# Install NPM packages inside Child Theme
+echo "This may take a minute or two...."
+# Build Child Theme CSS for the first time
+# Force RTL build
+
+# Exit UI
+echo "Done! You can start coding in: $themename"
+echo "===================================================="
